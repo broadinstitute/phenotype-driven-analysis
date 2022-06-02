785b67fd37759bf21020a35cb83cff4f878acec1
bw2 <ben.weisburd@gmail.com>
2022-06-02 15:49:55 -0400

    Add (for now empty) scripts for converting LIRICAL and Exomiser outputs to a format that can be uploaded to seqr (once that feature is implemented)

---
+++ seqr/convert_exomiser_json_to_seqr_format.py
@@ -0,0 +1,3 @@
+"""This script takes the Exomiser output json file and converts it to a format that can be uploaded to seqr (pending impelmentation of https://github.com/broadinstitute/seqr/issues/2742)
+
+import json
